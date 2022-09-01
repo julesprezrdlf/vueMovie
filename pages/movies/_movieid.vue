4e6d8829a332a455098a7e474161fb6bcb5a4586
julesprezrdlf <julioprezrdlf@gmail.com>
2022-09-01 21:23:57 +0200

    transitions and null error fix single movie

--- pages/movies/_movieid.vue
+++ pages/movies/_movieid.vue
@@ -13,7 +13,7 @@ export default {
     name: 'single-movie',
     data() {
         return{
-            movie: null,
+            movie: '',
 
         }
     },
@@ -26,7 +26,6 @@ export default {
             const data = axios.get(`https://api.themoviedb.org/3/movie/${this.$route.params.movieid}?api_key=9d5bbddde93fb73ec32279ed6f8795c3&language=en-US`)
             const result = await data
             this.movie = result.data
-            console.log(this.movie)
         }
     }
 }
