6cb3f09f107de58291f1f053a1d44ff4d21a7305
julioprezrdlf@gmail.com <julioprezrdlf@gmail.com>
2023-01-28 10:46:17 +0100

    starting store

--- store/index.js
+++ store/index.js
@@ -1,15 +1,35 @@
 /*eslint-disable*/
 
 export const state = () => ({
-  dark: false
+  /*dark mode*/
+  dark: false,
+
+  /*cart*/
+  movies: []
 });
 
 export const getters = {
+  /*dark mode*/
   dark: (state) => state.dark
+
+  /*cart*/
 };
 
 export const mutations = {
+  /*dark mode*/
   SET_DARK: (state, bool) => {
     state.dark = bool;
-  }
+
+
+  },
+
+  /*cart*/
+  ADD_MOVIE(state, movie) {
+    state.movies.push({ content: movie, done: false });
+  },
+  REMOVE_MOVIE(state, movie) {
+    state.movies.splice(state.movies.indexOf(movie), 1);
+  },
 };
+
+/*from https://thenextbit.de/en/blog/tailwind-dark-mode-toggle-nuxt-vuex */
